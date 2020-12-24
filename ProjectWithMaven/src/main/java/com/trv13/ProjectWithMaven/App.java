c7c69a367bee5bc70a2651267382b7e61d6e7d75
suryanarayan.rath <suryanarayan.rath@highradius.com>
2020-12-24 16:27:28 +0530

    Added Hibernate using Simple Maven

---
+++ ProjectWithMaven/src/main/java/com/trv13/ProjectWithMaven/App.java
@@ -0,0 +1,13 @@
+package com.trv13.ProjectWithMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
