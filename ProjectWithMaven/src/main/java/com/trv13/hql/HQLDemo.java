a7ee0c7ec923f012d8fc8c45a06c2948ffee5947
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-05 22:03:26 +0530

    Completed basic HQL

--- ProjectWithMaven/src/main/java/com/trv13/hql/HQLDemo.java
+++ ProjectWithMaven/src/main/java/com/trv13/hql/HQLDemo.java
@@ -1,9 +1,37 @@
 package com.trv13.hql;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.trv13.map.OneToOne.Question;
+
 public class HQLDemo {
-	Configuration configuration = new Configuration();
-	configuration.cofigure("hibernate.cfg.xml");
-	
+	public static void main(String[] args) {
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		SessionFactory factory = configuration.buildSessionFactory();
+
+		Session session = factory.openSession();
+		// HQL
+		String query = "from Question"; // Select to get all the datas
+		String query1 = "from Question where id=344"; // Where clause
+		String query2 = "from Question where id=:x"; // Dynamic parameter in HQL
+
+		Query q = session.createQuery(query2);
+		q.setParameter("x", 123);
+		
+		List<Question> list = q.list();
+		for (Question question : list) {
+			System.out.println(
+					question.getId() + " : " + question.getQuestion() + " : " + question.getAnswer().getAnswer());
+		}
+		
+		// q.uniqueResult() :- This is the unique result from the relation 
+		session.close();
+		factory.close();
+	}
 }
