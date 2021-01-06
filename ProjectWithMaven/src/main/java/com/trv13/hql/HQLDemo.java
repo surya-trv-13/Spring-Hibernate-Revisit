241c36d4519def0461e4523f07af93c242c065c4
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-06 19:47:08 +0530

    Added Delete the HQL query

--- ProjectWithMaven/src/main/java/com/trv13/hql/HQLDemo.java
+++ ProjectWithMaven/src/main/java/com/trv13/hql/HQLDemo.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.trv13.map.OneToOne.Question;
@@ -31,6 +32,16 @@ public class HQLDemo {
 		}
 		
 		// q.uniqueResult() :- This is the unique result from the relation 
+		
+		Transaction transaction = session.beginTransaction();
+		
+		// Delete HQL
+		Query q2 = session.createQuery("delete from Question where id=:id");
+		q2.setParameter("id", 344);
+		int rows = q2.executeUpdate();
+		System.out.println("Deleted :"+ rows);
+		
+		transaction.commit();
 		session.close();
 		factory.close();
 	}
