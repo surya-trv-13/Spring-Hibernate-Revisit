36500eed53d12c129794751500a1c88c9e60716e
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-08 19:00:31 +0530

    Another version of Second level Caching [Still failing]

--- ProjectWithMaven/src/main/java/com/trv13/cache/secondcache/SecondCacheDemo.java
+++ ProjectWithMaven/src/main/java/com/trv13/cache/secondcache/SecondCacheDemo.java
@@ -18,21 +18,15 @@ public class SecondCacheDemo {
 
 		Session session1 = factory.openSession();
 		// First Session
-		Query query1 = session1.createQuery("FROM Employee where employeeId=121");
-		query1.setCacheable(true);
-		query1.setCacheRegion("emp");
-		Employee employee = (Employee)query1.uniqueResult();
+		Employee employee = (Employee)session1.load(Employee.class, 121);
 		System.out.println(employee);
 		
 		session1.close();
 		
 		Session session2 = factory.openSession();
 		// Second Session
-		Query query2 = session2.createQuery("FROM Employee where employeeId=121");
-		query2.setCacheable(true);
-		query2.setCacheRegion("emp");
-		employee = (Employee)query2.uniqueResult();
-		System.out.println(employee);
+		Employee employee2 = (Employee)session2.load(Employee.class, 121);
+		System.out.println(employee2);
 		
 		session2.close();
 		
