cbf6c56f56c584e95ec6d3cd27e9d9c1e6b944f7
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-16 20:36:55 +0530

    Setup for Criteria API

---
+++ ProjectWithMaven/src/main/java/com/trv13/criteriaapi/CriteriaExample.java
@@ -0,0 +1,19 @@
+package com.trv13.criteriaapi;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CriteriaExample {
+	public static void main(String[] args) {
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		SessionFactory factory = configuration.buildSessionFactory();
+		Session session = factory.openSession(); 
+		
+		
+		
+		session.close();
+		factory.close();
+	}
+}
