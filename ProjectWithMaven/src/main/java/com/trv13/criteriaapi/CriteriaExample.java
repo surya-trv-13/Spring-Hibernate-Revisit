5a0e8265c8e5b3eff2929cb06da8d1d7b0b5ccaf
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-16 23:22:43 +0530

    Made changes for Criteria API

--- ProjectWithMaven/src/main/java/com/trv13/criteriaapi/CriteriaExample.java
+++ ProjectWithMaven/src/main/java/com/trv13/criteriaapi/CriteriaExample.java
@@ -1,8 +1,14 @@
 package com.trv13.criteriaapi;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+
+import com.trv13.map.ManyToMany.Employee;
 
 public class CriteriaExample {
 	public static void main(String[] args) {
@@ -11,7 +17,14 @@ public class CriteriaExample {
 		SessionFactory factory = configuration.buildSessionFactory();
 		Session session = factory.openSession(); 
 		
+		Criteria criteria = session.createCriteria(Employee.class);
+//		criteria.add(Restrictions.eq("id", 121)); // Only with data with id = 121
+		criteria.add(Restrictions.eq("project.projectName", "Autonomous Cloud"));
+		List<Employee> list = criteria.list();
 		
+		for(Employee e : list) {
+			System.out.println(e);
+		}
 		
 		session.close();
 		factory.close();
